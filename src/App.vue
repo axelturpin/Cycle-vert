087ef1998ef7a94f20f3deadcd71bd859461d194
axelturpin <axelturpin@hotmail.com>
2026-04-22 15:31:30 +0200

    Supprimer le dossier nom_du_dossier

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup></script>
+
+<template>
+  <router-view/>
+  <div>
+
+    <h1>You did it!</h1>
+    <p>
+      Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+      documentation
+    </p>
+    <p>or not</p>
+    <router-link to="/">Accueil</router-link>
+    <router-link to="/connexion">Connexion</router-link>
+  </div>
+  
+  
+</template>
+
+<style scoped></style>
---
+++ src/router/index.js
@@ -0,0 +1,26 @@
+import App from "@/App.vue";
+import Connexion from "@/components/Connexion.vue";
+import { createRouter, createWebHashHistory } from "vue-router";
+
+//définir les routes
+
+const roads = [
+    {
+        path: "/connexion",
+        name: "connexion",
+        component: Connexion
+    },
+    {
+        path: "/",
+        name: "App",
+        component: App
+    },
+]
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes: roads
+})
+
+
+export default router
